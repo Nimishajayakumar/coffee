41770a4ab56e754cd3664d5dbb77257a677a9d10
Nimishajayakumar <nimishaj2002@gmail.com>
2024-09-19 12:22:31 +0530

    second commit

--- main.js
+++ main.js
@@ -3,3 +3,16 @@ let search =document.querySelector('.search-box');
 document.querySelector('#search-icon').onclick = () => {
     search.classList.toggle('active');
 }
+
+let navabr =document.querySelector('.navbar');
+
+document.querySelector('#menu-icon').onclick = () => {
+    navabr.classList.toggle('active');
+}
+
+
+let header = document.querySelector('header');
+
+window.addEventListener('scroll' , ()=> {
+header.classList.toggle('shadow',window,scrollY > 0 );
+})
